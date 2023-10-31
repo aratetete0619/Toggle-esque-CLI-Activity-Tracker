74da1c09b52695020e6d6c756f2f93c40b85fefb
teteteo619 <120061560+aratetete0619@users.noreply.github.com>
2023-11-01 01:08:12 +0900

    Update track.sh

--- track.sh
+++ track.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker compose run app
+
+if [ -z "$TRACKER_PROJECT_DIR" ]; then
+    echo "Error: TRACKER_PROJECT_DIR environment variable is not set. Please run the install.sh script first."
+    exit 1
+fi
+
+cd "$TRACKER_PROJECT_DIR" && docker compose run app
